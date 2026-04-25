bf26ebf4a49e0e463eca35f50e7de69fc405ebba
MrQin877 <kuanchinzhong@gmail.com>
2026-04-26 03:54:59 +0800

    update setting

--- ngo_service/settings.py
+++ ngo_service/settings.py
@@ -14,6 +14,8 @@ from datetime import timedelta
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,10 +90,10 @@ WSGI_APPLICATION = 'ngo_service.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+    )
 }
 
 
