ad33b3056dfea858b432593ef37ddd3a9f8380f5
MrQin877 <kuanchinzhong@gmail.com>
2026-04-26 02:14:02 +0800

    Update settings.py

--- ngo_service/settings.py
+++ ngo_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'serviceday-local-dev-secret-key-2026'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('NGO_SERVICE_URL', '127.0.0.1 localhost').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
 
 REGISTRATION_SERVICE_URL = os.environ.get(
     'REGISTRATION_SERVICE_URL', 'http://127.0.0.1:8003'
